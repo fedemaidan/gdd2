00acb4623572c0e7bc28e2fcf04b2a327393f94a
Agustin <agustin.pace@hotmail.com>
2015-06-16 21:00:20 -0300

    resolvemos conflicto en ABM Cuentas

--- Aplicacion Desktop/PagoElectronico/PagoElectronico/ABM Cuenta/ABM_Cuenta.cs
+++ Aplicacion Desktop/PagoElectronico/PagoElectronico/ABM Cuenta/ABM_Cuenta.cs
@@ -1,5 +1,4 @@
-<<<<<<< Updated upstream
-﻿using System;
+
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,6 +8,7 @@ using System.Text;
 using System.Windows.Forms;
 using PagoElectronico.Model;
 using PagoElectronico.ABM_de_Usuario;
+using System;
 
 
 namespace PagoElectronico.ABM_Cuenta
@@ -19,142 +19,6 @@ namespace PagoElectronico.ABM_Cuenta
         {
             InitializeComponent();
 
-            Database db = new Database();
-            DataTable dt = new DataTable();
-
-            if (user.rol != "Administrador")
-            {
-                this.cb_clientes.Visible = false;
-                this.label5.Visible = false;
-            }
-            else
-            {
-                string query_user = "select u.nombre_usuario from qwerty.clientes c  join qwerty.usuarios u  on u.nombre_usuario = c.nombre_usuario;";
-                db = new Database();
-                dt = new DataTable();
-                dt = db.select_query(query_user);
-                foreach (DataRow row in dt.Rows)
-                {
-                    cb_clientes.Items.Add(row["nombre_usuario"].ToString());
-
-                }
-            }
-
-
-            Dia dia = new Dia();
-            textBox_fecha.Text = dia.Hoy().ToString("yyyy-MM-dd");
-            this.user=user;
-            
-            //agrego paises  al combobox
-            string qeri_paises = "select p.desc_pais from qwerty.paises p";
-            db = new Database();
-            dt = new DataTable();
-            dt = db.select_query(qeri_paises);
-            foreach (DataRow row in dt.Rows)
-            {
-                comboBox_pais.Items.Add(row["desc_pais"].ToString());   
-
-            }
-            //  termino de agregar paises al combobox
-
-            //agrego tipos de cuentas  al combobox
-            string qeri_tiposcuentas = "select c.descripcion from qwerty.categorias_de_cuentas c";
-            
-            dt = db.select_query(qeri_tiposcuentas);
-            foreach (DataRow row in dt.Rows)
-            {
-                comboBox_tipocuenta.Items.Add(row["descripcion"].ToString());
-
-            }
-            //  termino de agregar tipos de cuentas al combobox
-
-            //agrego monedas  al combobox
-            string qeri_monedas = "select m.descripcion from qwerty.monedas m";
-
-            dt = db.select_query(qeri_monedas);
-            foreach (DataRow row in dt.Rows)
-            {
-                comboBox_moneda.Items.Add(row["descripcion"].ToString());
-               
-            }
-            //  termino de agregar monedas al combobox
-
-        }
-        private User user;
-        
-        private int id_cliente;
-
-        private void ABM_Cuenta_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string qeri_buscoIDcliente;
-            if (user.rol != "Administrador")
-            {
-                qeri_buscoIDcliente = "select c.cliente_id from qwerty.clientes c where c.nombre_usuario='" + user.getUserName() + "'";
-            }
-            else
-            {
-                qeri_buscoIDcliente = "select c.cliente_id from qwerty.clientes c where c.nombre_usuario='" + cb_clientes.SelectedItem.ToString()  + "'";
-            }
-
-            
-            Database db = new Database();
-            
-
-            // tengo q buscar cliente_id en la tabla clientes con el nombre_usuario
-            
-            DataTable dt = db.select_query(qeri_buscoIDcliente);
-            foreach (DataRow row in dt.Rows)
-            {
-                this.id_cliente = Convert.ToInt32(row["cliente_id"]);
-            }
-            //termino de buscar el cliente_id
-            
-            // IMPORTANTE
-            // el numero_cuenta lo meto hardcodeado porque eso hay qe resolverlo en la tabla
-            // agregando al campo numero_cuenta la propiedad para que sea autoincrementable
-            //Random r = new Random();
-            //int aleatorio = r.Next(1,10000000);
-
-            // busco codigo del pais
-            string qeri_cod_pais = "select p.cod_pais from qwerty.paises p where p.desc_pais='"+comboBox_pais.SelectedItem.ToString()+"'";
-            dt = db.select_query(qeri_cod_pais);
-            int cod_pais = 0;
-            foreach (DataRow row in dt.Rows) {
-                cod_pais = Convert.ToInt32(row["cod_pais"]);
-            }
-            //termino de buscar codigo del pais
-            string qeri_duracion = "select cc.duracion from qwerty.categorias_de_cuentas cc where cc.descripcion='"+comboBox_tipocuenta.SelectedItem.ToString()+"'";
-            dt=db.select_query(qeri_duracion);
-            int duracion = 0;
-            foreach(DataRow row in dt.Rows){
-                duracion = Convert.ToInt32(row["duracion"]);
-            }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using PagoElectronico.Model;
-using PagoElectronico.ABM_de_Usuario;
-
-
-namespace PagoElectronico.ABM_Cuenta
-{
-    public partial class ABM_Cuenta : Form
-    {
-        public ABM_Cuenta(User user)
-        {
-            InitializeComponent();
->>>>>>> Stashed changes
             Dia dia = new Dia();
             textBox_fecha.Text = dia.Hoy().ToString("yyyy-MM-dd");
             this.user=user;
